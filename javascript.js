9c62701fb8d9ad4a6d5bbd78e63f02f0f27cb9f0
Maxwell Hurst <maxhurst120898@gmail.com>
2024-09-12 21:13:44 -0400

    HTML, CSS, Javascript updates

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let firstName = "Maxwell";
+let lastName = "Hurst";
+const fullName = firstName + lastName;
+let numberOfApples = 5;
+let numberOfOranges = 10;
+if (numberOfApples < numberOfOranges) {
+    console.log("There is an imbalance in the ratio of Apples to Oranges.")
+};
